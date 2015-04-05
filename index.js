764083de0524a73c3da4e51777fe2d16493722aa
tunnckoCore <mameto_100@mail.bg>
2015-04-05 17:06:59 +0300

    first commits

---
+++ index.js
@@ -0,0 +1,30 @@
+/**
+ * jstransformer-xml2js <https://github.com/tunnckoCore/jstransformer-xml2js>
+ *
+ * Copyright (c) 2015 Charlike Mike Reagent, contributors.
+ * Released under the MIT license.
+ */
+
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var xml2js = require('xml2js');
+var bluebird = require('bluebird');
+var readFile = bluebird.promisify(fs.readFile);
+var readFileSync = fs.readFileSync;
+
+exports.name = 'xml2js';
+exports.outputFormat = 'json';
+
+exports.renderAsync = function _renderAsync(str, opts) {
+  var parseString = bluebird.promisify(xml2js.parseString);
+  return parseString(str, opts).then(function(data) {
+    return data;
+  });
+};
+exports.renderFileAsync = function _renderFileAsync(filepath, opts) {
+  return readFile(filepath, 'utf8').then(function(data) {
+    return exports.renderAsync(data, opts);
+  });
+};
---
+++ test/index.js
@@ -0,0 +1,30 @@
+/**
+ * jstransformer-xml2js <https://github.com/tunnckoCore/jstransformer-xml2js>
+ *
+ * Copyright (c) 2015 Charlike Mike Reagent, contributors.
+ * Released under the MIT license.
+ */
+
+'use strict';
+
+var test = require('assertit');
+var transform = require('../index');
+
+test('should render xml string asynchronously, (promise)', function(done) {
+  var fixture = '<?xml version="1.0" encoding="UTF-8"?><feed xmlns="http://www.w3.org/2005/Atom"><title>Example Feed</title></feed>';
+  var promise = transform.renderAsync(fixture);
+
+  promise.then(function(actual) {
+    test.deep.equal(actual.feed.title, ['Example Feed']);
+    done();
+  });
+});
+test('should render xml file asynchronously (promise)', function(done) {
+  var promise = transform.renderFileAsync('./test/fixture.xml');
+
+  promise.then(function(actual) {
+    test(actual);
+    test.deep.equal(actual['sdk:source']['sdk:revision'], ['2'])
+    done();
+  });
+});
