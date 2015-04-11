6e834bab7d4d24757e5a141259ba46d09641c7c0
tunnckoCore <mameto_100@mail.bg>
2015-04-11 16:34:52 +0300

    cleanup

--- index.js
+++ index.js
@@ -7,12 +7,8 @@
 
 'use strict';
 
-var fs = require('fs');
-var path = require('path');
 var xml2js = require('xml2js');
 var bluebird = require('bluebird');
-var readFile = bluebird.promisify(fs.readFile);
-var readFileSync = fs.readFileSync;
 
 exports.name = 'xml2js';
 exports.inputFormats = ['xml', 'xml2js'];
@@ -22,10 +18,5 @@ exports.renderAsync = function _renderAsync(str, opts) {
   var parseString = bluebird.promisify(xml2js.parseString);
   return parseString(str, opts).then(function(data) {
     return data;
-  });
-};
-exports.renderFileAsync = function _renderFileAsync(filepath, opts) {
-  return readFile(filepath, 'utf8').then(function(data) {
-    return exports.renderAsync(data, opts);
   });
 };
